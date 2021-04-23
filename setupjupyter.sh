c58b64f5a3475333bd1620e8abb39083998887f1
Justin Guese <guese.justin@gmail.com>
2021-04-23 18:15:15 +0200

    first two sections

---
+++ setupjupyter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# further details: https://towardsdatascience.com/how-to-set-up-anaconda-and-jupyter-notebook-the-right-way-de3b7623ea4a
+conda install -c conda-forge notebook
+conda install -c conda-forge nb_conda_kernels
+conda install -c conda-forge jupyter_contrib_nbextensions
+conda install pip 
+
